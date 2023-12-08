90571fcbb1886e7d025a0e05b6992cd4c82ab980
MateoBaltar <mateobaltar@gmail.com>
2023-12-08 19:56:30 -0300

    [FIX] Services Menu to be last to load in manifest

--- gradein_services/__manifest__.py
+++ gradein_services/__manifest__.py
@@ -9,9 +9,9 @@
         'security/groups.xml',
         'security/ir.model.access.csv',
         'views/gradein_answer_view.xml',
-        'views/menu_services.xml',
         'views/gradein_question_view.xml',
         'views/equipment_type_view.xml',
         'views/gradein_order_view.xml',
+        'views/menu_services.xml',
     ]
 }
